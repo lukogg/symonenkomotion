32b922b35598d9cc49fb9e3a546d6f24d7a17be3
luko <simonenkorostik@gmail.com>
2026-08-03 15:22:45 +0300

    misc

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 import App from './App.jsx'
 import './index.css'
 
